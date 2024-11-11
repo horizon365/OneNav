5c68447adac24e35754ab0e53ab0194e5a96b418
yiqun <sunnylyq@163.com>
2024-11-11 17:36:46 +0800

    调整位置

--- gitpod.sh
+++ gitpod.sh
@@ -1,4 +1,2 @@
 npm i -g geneasy
 geneasy -t index.hbs -o index.html links.yml
-npm i -g http-server
-http-server -p 8000
--- vercel.sh
+++
@@ -1,2 +0,0 @@
-npm i -g geneasy
-geneasy -t index.hbs -o index.html links.yml
